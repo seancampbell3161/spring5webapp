8d09118756d97aa2cc93886eb5bf3a07cbe80d6e
Sean Campbell <sean.campbell3161@gmail.com>
2021-07-23 15:54:26 -0500

    stuff

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -1,16 +1,10 @@
 package guru.springframework.spring5webapp.repositories;
 
-<<<<<<< HEAD
 import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
-=======
-import guru.springframework.spring5webapp.domain.Book;
-import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by jt on 12/23/19.
- */
->>>>>>> add-publisher
+
+
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
